5667089d0faee780c2779b286fbd769aae896fc5
Michael Ogala <michael.ogala@andela.com>
2016-11-04 12:47:14 +0100

    add create method

--- lib/modularize.rb
+++ lib/modularize.rb
@@ -1,5 +1,24 @@
 require "modularize/version"
 
 module Modularize
-  # Your code goes here...
+  def self.create(path, root = Object)
+    path.split('::').each do |name|
+      root = create_module(name, root)
+    end
+  end
+
+  private
+
+  def self.create_module(name, root)
+    new_module = if root.const_defined?(name)
+               root.const_get(name)
+             else
+               nil
+             end
+    unless new_module
+      new_module = Module.new
+      root.const_set name, new_module
+    end
+    new_module
+  end
 end
--- spec/modularize_spec.rb
+++ spec/modularize_spec.rb
@@ -5,7 +5,22 @@ describe Modularize do
     expect(Modularize::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  describe '.create' do
+
+    context 'with single module name as argument' do
+      it 'creates a single module' do
+        expect(defined? Fruit).to be_nil
+        Modularize.create('Fruit')
+        expect(defined? Fruit).to_not be_nil
+      end
+    end
+
+    context 'with more than one module name in the args string' do
+      it 'creates a hierarchy of modules' do
+        expect(defined?(Animals::Cats::Bombay)).to be_nil
+        Modularize.create('Animals::Cats::Bombay')
+        expect(defined?(Animals::Cats::Bombay)).to_not be_nil
+      end
+    end
   end
 end
