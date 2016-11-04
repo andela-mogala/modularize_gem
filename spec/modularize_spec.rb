4dd961734969a33b8cb1d8f760e42388973adc9d
Michael Ogala <michael.ogala@andela.com>
2016-11-04 12:00:47 +0100

    initialize gem

---
+++ spec/modularize_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe Modularize do
+  it 'has a version number' do
+    expect(Modularize::VERSION).not_to be nil
+  end
+
+  it 'does something useful' do
+    expect(false).to eq(true)
+  end
+end
